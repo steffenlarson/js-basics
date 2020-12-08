22dbe10ab2495f39d529b55c4a2c2715efe2cf28
Steffen Larson <steffman13@gmail.com>
2020-12-08 13:05:38 -0700

    got to conditionals

--- app/conditionals.js
+++ app/conditionals.js
@@ -2,7 +2,7 @@
 
 function largerNum(num1, num2) {
     // you code here
-
+    Math.max(num1, num2)
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
